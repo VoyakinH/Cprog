22d25d0bd0d9f0d93bcce78361f5ca5e3dac34e4
vaya18u192 <aleksei_voyakin@mail.ru>
2019-03-01 22:51:36 +0300

    spaces added 2

--- lab_01_2_1/main.c
+++ lab_01_2_1/main.c
@@ -4,7 +4,7 @@
 int main(void)
 {
     float x1, y1, x2, y2, x3, y3, s;
-    scanf("%f%f%f%f%f%f" , &x1, &y1, &x2, &y2, &x3, &y3);
+    scanf("%f%f%f%f%f%f", &x1, &y1, &x2, &y2, &x3, &y3);
     s = 0;
     s += sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
     s += sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
