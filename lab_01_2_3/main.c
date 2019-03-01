1d6f2ef3a0d62c637f12ea7c5ca5d51036795b8a
vaya18u192 <aleksei_voyakin@mail.ru>
2019-03-01 23:31:01 +0300

    third programm added

---
+++ lab_01_2_3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float r1, r2, r3;
+    scanf("%f%f%f", &r1, &r2, &r3);
+    r1 = 1/r1;
+    r1 += 1/r2;
+    r1 += 1/r3;
+    r1 = 1/r1;
+    printf("%f", r1);
+    return 0;
+}
