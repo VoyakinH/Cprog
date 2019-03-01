95f7bb3a0ecb637abe6172dfc8939b2b9a17c2d1
vaya18u192 <aleksei_voyakin@mail.ru>
2019-03-01 22:48:32 +0300

    spaces added

--- lab_01_2_1/main.c
+++ lab_01_2_1/main.c
@@ -3,12 +3,12 @@
 
 int main(void)
 {
-    float x1,y1,x2,y2,x3,y3,s;
-    scanf("%f%f%f%f%f%f",&x1,&y1,&x2,&y2,&x3,&y3);
+    float x1, y1, x2, y2, x3, y3, s;
+    scanf("%f%f%f%f%f%f" , &x1, &y1, &x2, &y2, &x3, &y3);
     s = 0;
-    s += sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-    s += sqrt((x2-x3)*(x2-x3)+(y2-y3)*(y2-y3));
-    s += sqrt((x1-x3)*(x1-x3)+(y1-y3)*(y1-y3));
-    printf("%.5f",s);
+    s += sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    s += sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+    s += sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+    printf("%.5f", s);
     return 0;
 }
