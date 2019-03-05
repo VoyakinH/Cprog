5a6961259152716862b3e2273a68054d8c699491
vaya18u192 <aleksei_voyakin@mail.ru>
2019-03-05 16:16:14 +0300

    style corrected in first programm

--- lab_01_0_1/main.c
+++ lab_01_0_1/main.c
@@ -5,12 +5,12 @@ int main(void)
 {
     float a, b, h;
 	scanf("%f%f%f", &a, &b, &h);
-if (a == 0 || b == 0 || h == 0)
-{
-	a = 0;
-	b = 0;
-	h = 0;
-}
+	if (a == 0 || b == 0 || h == 0)
+	{
+		a = 0;
+		b = 0;
+		h = 0;
+	}
 	h = sqrt(((fabs(a - b) / 2) * (fabs(a - b) / 2)) + (h * h));
 	h = h + h + a + b;
     printf("%.5f", h);
