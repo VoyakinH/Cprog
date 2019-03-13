0956ee62b6edd0dd5d5f1d6bf03b7d4de7b9b375
vaya18u192 <aleksei_voyakin@mail.ru>
2019-03-13 22:54:06 +0300

    Added check for input data for second programm

--- lab_02_1_2/main.c
+++ lab_02_1_2/main.c
@@ -4,8 +4,8 @@
 int main(void)
 {
     float rc, a, b, c, p, s;
-    int x1, x2, x3, y1, y2, y3;
-    rc = scanf("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &x3, &y3);
+    float x1, x2, x3, y1, y2, y3;
+    rc = scanf("%f%f%f%f%f%f", &x1, &y1, &x2, &y2, &x3, &y3);
     if (rc == 6)
     {
         a = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
@@ -13,8 +13,16 @@ int main(void)
         c = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2));
         p = (a + b + c) / 2;
         s = sqrt(p * (p - a) * (p - b) * (p - c));
-        printf("%.6f", s);
-        return 0;
+        if ((x1 == x2 && y1 == y2) || (x2 == x3 && y2 == y3) || (x1 == x3 && y1 == y3))
+        {
+            printf("Input error");
+            return 1;
+        }
+        else
+        {
+            printf("%.6f", s);
+            return 0;
+        }
     }
     else
     {
