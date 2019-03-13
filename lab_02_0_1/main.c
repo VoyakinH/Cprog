08b0b72604411cc6248802ff990fb892fa68dc45
vaya18u192 <aleksei_voyakin@mail.ru>
2019-03-13 21:36:39 +0300

    Added code error output and output is now int

--- lab_02_0_1/main.c
+++ lab_02_0_1/main.c
@@ -3,11 +3,19 @@
 
 int main(void)
 {
-    int a, n, rc;
+    int a, n, rc, s;
     rc = scanf("%d %d", &a, &n);
+    s = 1;
     if (rc == 2 && n > 0)
-        printf("%.6f", pow(a, n));
+    {
+        for (int i = 1; i <= n; i++)
+            s *= a;
+        printf("%d", s);
+        return 0;
+    }
     else
+    {
         printf("Input error");
-    return 0;
+        return 1;
+    }
 }
