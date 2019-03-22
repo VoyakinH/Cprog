7ebaf35b39b30c8ec534ba3664b21a90e38d3408
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-03-22 16:12:31 +0300

    First initial programm redone with correct variant

--- lab_04_2_1/main.c
+++ lab_04_2_1/main.c
@@ -3,33 +3,30 @@
 
 int main(void)
 {
-    int n, i, ls;
-    float s, k;
+    int n, i, rs, k;
+    float s;
     char tmp;
     scanf("%d", &n);
     if (n < 0 || n > 10)
         return 1;
     int array[n];
-    ls = 0;
+    rs = 0;
     for (i = 0; i < n; i++)
-        ls += scanf("%d", &array[i]);
+        rs += scanf("%d", &array[i]);
     scanf("%c", &tmp);
-    if (tmp != '\n')
+    if (rs != n || tmp != '\n')
         return 1;
-    if (ls != n)
-        return 1;
-    s = 1;
     k = 0;
+    s = 0;
     for (i = 0; i < n; i++)
-        if (array[i] > 0)
+        if (array[i] < 0)
         {
-            s *= array[i];
+            s += array[i];
             k++;
         }
-    if (k == 0 )
+    if (k == 0)
         return 1;
-    k = 1 / k;
-    s = pow(s, k);
-    printf("%.6f", s);
+    s = s / k;
+    printf("%.6f\n", s);
     return 0;
 }
