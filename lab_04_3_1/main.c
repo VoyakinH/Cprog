5f1eacf58c766d86916f9387de2172143c3617b2
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-03-22 14:43:26 +0300

    Check #2 added

--- lab_04_3_1/main.c
+++ lab_04_3_1/main.c
@@ -5,6 +5,7 @@ int main(void)
 {
     int n, i, ls;
     float s, k;
+    char tmp;
     scanf("%d", &n);
     if (n < 0 || n > 10)
         return 1;
@@ -12,6 +13,9 @@ int main(void)
     ls = 0;
     for (i = 0; i < n; i++)
         ls += scanf("%d", &array[i]);
+    scanf("%c", &tmp);
+    if (tmp != '\n')
+        return 1;
     if (ls != n)
         return 1;
     s = 1;
