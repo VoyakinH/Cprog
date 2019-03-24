b880af572ece811b185803f75ecfba68a6539c72
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-03-25 00:38:06 +0300

    Try to do first programm 16

--- lab_04_2_1/main.c
+++ lab_04_2_1/main.c
@@ -14,19 +14,15 @@ int main(void)
 {
     int n, i, rs1, rs2;
     scanf("%d", &n);
-    if (n == 0)
-        return 0;
     int array[n];
     rs1 = 0;
     rs2 = 0;
     for (i = 0; i < n; i++)
     {
-        rs1 += scanf("%d", &array[i]);
+        scanf("%d", &array[i]);
         if (array[i] < 0)
             rs2++;
     }
-    if (rs1 != n || rs2 == 0)
-        return 1;
     srar(array, n, rs2);
     return 0;
 }
