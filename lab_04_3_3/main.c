3e1a0aea708043ca6862c3d941e7c28e68bfffe2
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-03-25 12:25:03 +0300

    Try to do first programm 12

--- lab_04_3_3/main.c
+++ lab_04_3_3/main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
-void vstav(int array[], int n)
+int main(void)
 {
-    int rs, kolpol, j;
-    int i = 0;
+    int n, i, kolpol, rs, j;
+    scanf("%d", &n);
+    if (n <= 0 || n > 10)
+        return 1;
+    int array[n];
+    kolpol = 0;
+    rs = 0;
+    for (i = 0; i < n; i++)
+    {
+        rs += scanf("%d", &array[i]);
+        if (array[i] > 0)
+            kolpol = 1;
+    }
+    if (kolpol == 0 || rs != n || rs == EOF)
+        return 1;
+    i = 0;
     while (i < n)
     {
         if (array[i] > 0)
@@ -27,27 +41,5 @@ void vstav(int array[], int n)
     }
     for (i = 0; i < n; i++)
         printf("%d ", array[i]);
-    printf("\n");
-    return;
-}
-
-int main(void)
-{
-    int n, i, kolpol, rs;
-    scanf("%d", &n);
-    if (n <= 0 || n > 10)
-        return 1;
-    int array[n];
-    kolpol = 0;
-    rs = 0;
-    for (i = 0; i < n; i++)
-    {
-        rs += scanf("%d", &array[i]);
-        if (array[i] > 0)
-            kolpol = 1;
-    }
-    if (kolpol == 0 || rs != n || rs == EOF)
-        return 1;
-    vstav(array, n);
     return 0;
 }
