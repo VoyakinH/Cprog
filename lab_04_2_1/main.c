4407332338cf99fa2389948ca323ca4341eb71f5
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-03-27 16:44:51 +0300

    qt files added

--- lab_04_2_1/main.c
+++ lab_04_2_1/main.c
@@ -2,27 +2,27 @@
 
 void srar(int array[], int n, int rs2)
 {
-    float s = 0;
+    double s = 0;
     for (int i = 0; i < n; i++)
         if (array[i] < 0)
             s += array[i];
     s = s / rs2;
-    printf("%f\n", s);
+    printf("%lf\n", s);
 }
 
 int main(void)
 {
-    int n, i, rs2;
-    scanf("%d", &n);
+    int n, i, rs2, rs1;
+    rs1 = scanf("%d", &n);
     int array[n];
     rs2 = 0;
     for (i = 0; i < n; i++)
     {
-        scanf("%d", &array[i]);
+        rs1 += scanf("%d", &array[i]);
         if (array[i] < 0)
             rs2++;
     }
-    if (rs2 == 0)
+    if (rs2 == 0 || rs1 != n || rs1 == EOF)
         return 1;
     srar(array, n, rs2);
     return 0;
