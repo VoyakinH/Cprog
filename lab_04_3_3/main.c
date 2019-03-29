b2ea5e09030e45862e88c6fc3fbbfac4cd8a61e5
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-03-29 20:35:37 +0300

    Main function in third programm splitted

--- lab_04_3_3/main.c
+++ lab_04_3_3/main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
+int n_input()
+{
+    int rs, n;
+    rs = scanf("%d", &n);
+    if (rs != 1 || n > 10 || n <= 0)
+        return -1;
+    return n;
+}
+
+int arr_input(int n, int array[])
+{
+    int rs;
+    int kolpol = 0;
+    for (int i = 0; i < n; i++)
+    {
+        rs = scanf("%d", &array[i]);
+        if (array[i] > 0)
+            kolpol++;
+        if (rs != 1 || rs == EOF)
+            return -1;
+    }
+    if (kolpol == 0)
+        return -1;
+    return 1;
+}
+
 void sdvig(int array[], int n, int i, int x)
 {
     for (int rs = n; rs > i + 1; rs--)
@@ -22,24 +48,22 @@ int reverse(int array[], int i)
     return kolpol;
 }
 
+void arr_output(int array[], int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", array[i]);
+    return;
+}
+
 int main()
 {
-    int n, i, kolpol, rs;
-    rs = scanf("%d", &n);
-    if (n <= 0 || n > 10 || rs != 1)
+    int n, i, rs;
+    n = n_input();
+    if (n == -1)
         return 1;
-    int array[2 * n];
-    kolpol = 0;
-    rs = 0;
-    for (i = 0; i < n; i++)
-    {
-        rs = scanf("%d", &array[i]);
-        if (rs != 1)
-            return 1;
-        if (array[i] > 0)
-            kolpol = 1;
-    }
-    if (kolpol == 0)
+    int array[n];
+    rs = arr_input(n, array);
+    if (rs == -1)
         return 1;
     i = 0;
     while (i < n)
@@ -52,7 +76,6 @@ int main()
         }
         i++;
     }
-    for (i = 0; i < n; i++)
-        printf("%d ", array[i]);
+    arr_output(array, n);
     return 0;
 }
