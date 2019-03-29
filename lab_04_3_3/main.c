e9ca9154cc142f8f22a7402a5d340374f934d9c2
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-03-29 21:46:17 +0300

    Main function in third programm splitted

--- lab_04_3_3/main.c
+++ lab_04_3_3/main.c
@@ -2,9 +2,10 @@
 
 int n_input()
 {
-    int rs, n;
-    rs = scanf("%d", &n);
-    if (rs != 1 || n > 10 || n <= 0)
+    int n;
+    if (scanf("%d", &n) != 1)
+        return -1
+    if (n > 10 || n <= 0)
         return -1;
     return n;
 }
@@ -12,17 +13,12 @@ int n_input()
 int arr_input(int n, int array[])
 {
     int rs;
-    int kolpol = 0;
     for (int i = 0; i < n; i++)
     {
         rs = scanf("%d", &array[i]);
-        if (array[i] > 0)
-            kolpol++;
         if (rs != 1 || rs == EOF)
             return -1;
     }
-    if (kolpol == 0)
-        return -1;
     return 1;
 }
 
