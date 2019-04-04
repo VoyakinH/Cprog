b709ec4a59e5aee51e807c6b647a8f0372a4985f
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-04-05 00:01:38 +0300

    Test 2

--- lab_05_0_1/main.c
+++ lab_05_0_1/main.c
@@ -25,7 +25,7 @@ void arr_check(int array[N][N], int arr1[], int k, int n, int m)
             l++;
         }
     for (i = 0; i < n - 1; i++)
-        if (array[i][k] < array[i + 1][k])
+        if (array[i][k - 1] < array[i + 1][k - 1])
         {
             arr1[k - 1] = 0;
             return;
