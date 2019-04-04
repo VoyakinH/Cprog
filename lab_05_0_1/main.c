45a39ba3af69cd82e5c1fa6fd3e7caf95726e5e5
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-04-05 01:04:23 +0300

    Test 9

--- lab_05_0_1/main.c
+++ lab_05_0_1/main.c
@@ -21,23 +21,25 @@ void arr_check(int array[N][N], int arr1[], int k, int n, int m)
     for (i = 0; i < n; i++)
         for (j = 0; j < m; j++)
         {
+            if (l == k)
+                l++;
             arr1[l] = array[i][j];
             l++;
         }
     for (i = 0; i < n - 1; i++)
         if (array[i][k] < array[i + 1][k])
         {
-            arr1[k + 1] = 0;
+            arr1[k] = 0;
             return;
         }
-    arr1[k + 1] = 1;
+    arr1[k] = 1;
     return;
 }
 
 void arr_print(int arr1[], int n, int m)
 {
     n *= m;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i <= n; i++)
         printf("%d ", arr1[i]);
     return;
 }
@@ -58,7 +60,7 @@ int main(void)
         printf("Данные введены неверно.");
         return 1;
     }
-    int arr1[n * m];
+    int arr1[n * m + 1];
     arr_check(array, arr1, k, n, m);
     arr_print(arr1, n, m);
     return 0;
