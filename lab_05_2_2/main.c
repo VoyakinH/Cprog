6171a15be41a45612f2b83bc571c075ad171516f
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-04-05 02:01:05 +0300

    Elements print in line in second programm

--- lab_05_2_2/main.c
+++ lab_05_2_2/main.c
@@ -58,11 +58,8 @@ void arr_print(int array[N][N], int n, int m)
 {
     int i, j;
     for (i = 0; i < n; i++)
-    {
         for (j = 0; j < m; j++)
             printf("%d ", array[i][j]);
-        printf("\n");
-    }
     return;
 }
 
