2b425211e9eb02cd04d1a27852145486bcd3bf5b
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-04-06 11:02:58 +0300

    Function in first program redone

--- lab_05_0_1/main.c
+++ lab_05_0_1/main.c
@@ -14,37 +14,33 @@ int arr_input(int array[N][N], int n, int m)
     return 0;
 }
 
-void arr_check(int array[N][N], int arr1[], int k, int n, int m)
+void arr_check(int array[N][N], int arr1[N], int n, int m)
 {
-    int i, j;
-    int l = 0;
-    for (i = 0; i < n; i++)
-        for (j = 0; j < m; j++)
-        {
-            arr1[l] = array[i][j];
-            l++;
-        }
-    for (i = 0; i < n - 1; i++)
-        if (array[i][k] < array[i + 1][k])
-        {
-            arr1[k] = 0;
-            return;
-        }
-    arr1[k] = 1;
+    int i, j, ok;
+    for (i = 0; i < m; i++)
+    {
+        ok = 1;
+        for (j = 0; j < n - 1; j++)
+            if (array[j][i] < array[j + 1][i])
+                ok = 0;
+        if (ok == 1)
+            arr1[i] = 1;
+        else
+            arr1[i] = 0;
+    }
     return;
 }
 
-void arr_print(int arr1[], int n, int m)
+void arr_print(int arr1[], int m)
 {
-    n *= m;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < m; i++)
         printf("%d ", arr1[i]);
     return;
 }
 
 int main(void)
 {
-    int n, m, k, rs;
+    int n, m, rs;
     printf("Введите кол-во строк матрицы, столбцов матрицы: ");
     rs = scanf("%d %d", &n, &m);
     if (rs != 2 || n > 10 || m > 10 || n <= 0 || m <= 0)
@@ -58,15 +54,8 @@ int main(void)
         printf("Данные введены неверно.");
         return 1;
     }
-    printf("Введите K: ");
-    rs = scanf("%d", &k);
-    if (k >= m || k >= n || k < 0 || rs != 1)
-    {
-        printf("Данные введены неверно.");
-        return 1;
-    }
-    int arr1[N * N];
-    arr_check(array, arr1, k, n, m);
-    arr_print(arr1, n, m);
+    int arr1[N];
+    arr_check(array, arr1, n, m);
+    arr_print(arr1, m);
     return 0;
 }
