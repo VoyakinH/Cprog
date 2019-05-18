3b1a0aee7e3f64bcb10ded89a8f8e7baf4d5aa70
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-05-18 16:28:39 +0300

    Programm 1, 3 decomposed

--- lab_04_2_1/main.c
+++ lab_04_2_1/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int n_input()
+int n_input(int *n)
 {
-    int rs, n;
-    rs = scanf("%d", &n);
-    if (rs != 1 || n > 10 || n <= 0)
+    int rs;
+    rs = scanf("%d", &*n);
+    if (rs != 1 || *n > 10 || *n <= 0)
         return -1;
-    return n;
+    return 0;
 }
 
 int arr_input(int n, int array[])
@@ -18,38 +18,42 @@ int arr_input(int n, int array[])
         if (rs != 1 || rs == EOF)
             return -1;
     }
-    return 1;
+    return 0;
 }
 
-int srar(int array[], int n)
+int srar(int array[], int n, float *s)
 {
     int k = 0;
-    float s = 0;
+    *s = 0;
     for (int i = 0; i < n; i++)
         if (array[i] < 0)
         {
             k++;
-            s += array[i];
+            *s += array[i];
         }
     if (k == 0)
-        return 1;
-    s /= k;
-    printf("%.6f", s);
+        return -1;
+    *s /= k;
     return 0;
 }
 
-int main(void)
+void output(float s)
 {
-    int n, rs;
-    n = n_input();
-    if (n == -1)
+    printf("%.6f\n", s);
+    return;
+}
+
+int main()
+{
+    int n;
+    float s;
+    if (n_input(&n) == -1)
         return 1;
     int array[n];
-    rs = arr_input(n, array);
-    if (rs == -1)
+    if (arr_input(n, array) == -1)
         return 1;
-    rs = srar(array, n);
-    if (rs == 1)
+    if (srar(array, n, &s) == -1)
         return 1;
+    output(s);
     return 0;
 }
