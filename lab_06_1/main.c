b5a378b6a2988e5e5fc3f85d61904f2eb34fbda0
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-05-31 02:45:55 +0300

    Memory fix in first program

--- lab_06_1/main.c
+++ lab_06_1/main.c
@@ -39,9 +39,9 @@ int main(void)
 {
     char str1[N] = "abcd aowpf ";
     char str2[N] = "c w";
-    if (my_strpbrk(str1, str2) != strpbrk(str1, str2)
+    if (my_strpbrk(str1, str2) != strpbrk(str1, str2))
     {
-        printf("my_strpbrk : FAILED, ожидалось: %s, фактическое: %s", strpbrk(str1, str2, my_strpbrk(str1, str2));
+        printf("my_strpbrk : FAILED, ожидалось: %s, фактическое: %s", strpbrk(str1, str2), my_strpbrk(str1, str2));
         return 1;
     }
     return 0;
