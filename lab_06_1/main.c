39f8652c4c0aec3597e2074c11deeb61e334444a
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-05-31 21:27:51 +0300

    Advanced tests added in first prog

--- lab_06_1/main.c
+++ lab_06_1/main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-char *my_strpbrk(char *str1, char *str2)
+char *my_strpbrk(char *str11, char *str12)
 {
     int i = 0;
     char *rc;
     int founded = 0;
     while (1)
     {
-        if (str1[i] == '\0')
+        if (str11[i] == '\0')
             break;
         int j = 0;
         founded = 0;
         while (1)
         {
-            if (str2[j] == '\0')
+            if (str12[j] == '\0')
                 break;
-            if (str1[i] == str2[j])
+            if (str11[i] == str12[j])
             {
-                rc = &str1[i];
+                rc = &str11[i];
                 founded = 1;
                 break;
             }
@@ -33,24 +33,24 @@ char *my_strpbrk(char *str1, char *str2)
     return rc;
 }
 
-unsigned long int my_strspn(char *str1, char *str2)
+unsigned long int my_strspn(char *str21, char *str22)
 {
     int i = 0;
     int j;
     unsigned long  int count = 0;
     while (1)
     {
-        if (str1[i] == '\0')
+        if (str21[i] == '\0')
             break;
         j = 0;
         while(1)
         {
-            if (str2[j] == '\0')
+            if (str22[j] == '\0')
             {
                 return count;
                 break;
             }
-            if (str2[j] == str1[i])
+            if (str22[j] == str21[i])
             {
                 count++;
                 break;
@@ -65,16 +65,18 @@ unsigned long int my_strspn(char *str1, char *str2)
 
 int main()
 {
-    char str1[] = "201201 oqkdj";
-    char str2[] = "210";
-    if (my_strpbrk(str1, str2) != strpbrk(str1, str2))
+    char str11[] = "esfoi-1 ueh .,10 ! ;";
+    char str12[] = "2wef;.!-1 ";
+    if (my_strpbrk(str11, str12) != strpbrk(str11, str12))
     {
-        printf("my_strpbrk : FAILED, ожидалось: %s, фактическое: %s", strpbrk(str1, str2), my_strpbrk(str1, str2));
+        printf("my_strpbrk : FAILED, ожидалось: %s, фактическое: %s", strpbrk(str11, str12), my_strpbrk(str11, str12));
         return 1;
     }
-    if (my_strspn(str1, str2) != strspn(str1, str2))
+    char str21[] = "20112 f-1-1p.,! tpa";
+    char str22[] = "012f -1p.,!";
+    if (my_strspn(str21, str22) != strspn(str21, str22))
     {
-        printf("my_strspn : FAILED, ожидалось: %lu, фактическое: %lu", strspn(str1, str2), my_strspn(str1, str2));
+        printf("my_strspn : FAILED, ожидалось: %lu, фактическое: %lu", strspn(str21, str22), my_strspn(str21, str22));
         return 2;
     }
     return 0;
