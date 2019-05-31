4cbf6fef7be8c893554dcae4a00c87b426596e20
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-05-31 21:04:24 +0300

    my_strspn added in first program

--- lab_06_1/main.c
+++ lab_06_1/main.c
@@ -33,15 +33,49 @@ char *my_strpbrk(char *str1, char *str2)
     return rc;
 }
 
-
-int main(void)
+unsigned long int my_strspn(char *str1, char *str2)
 {
-    char str1[] = "abcd aowpf ";
-    char str2[] = "c w";
+    int i = 0;
+    int j;
+    unsigned long  int count = 0;
+    while (1)
+    {
+        if (str1[i] == '\0')
+            break;
+        j = 0;
+        while(1)
+        {
+            if (str2[j] == '\0')
+            {
+                return count;
+                break;
+            }
+            if (str2[j] == str1[i])
+            {
+                count++;
+                break;
+            }
+            j++;
+        }
+        i++;
+    }
+    return count;
+}
+
+
+int main()
+{
+    char str1[] = "201201 oqkdj";
+    char str2[] = "210";
     if (my_strpbrk(str1, str2) != strpbrk(str1, str2))
     {
         printf("my_strpbrk : FAILED, ожидалось: %s, фактическое: %s", strpbrk(str1, str2), my_strpbrk(str1, str2));
         return 1;
+    }
+    if (my_strspn(str1, str2) != strspn(str1, str2))
+    {
+        printf("my_strspn : FAILED, ожидалось: %lu, фактическое: %lu", strspn(str1, str2), my_strspn(str1, str2));
+        return 2;
     }
     return 0;
 }
