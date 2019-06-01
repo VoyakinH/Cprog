b0964a0b7171ce1f7917e4939da244f1af579310
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-06-01 10:11:53 +0300

    Memory fix # 4 in second program

--- lab_06_2_2/main.c
+++ lab_06_2_2/main.c
@@ -102,6 +102,7 @@ int main()
 {
     char str1[N];
     char str2[N];
+    int n2;
     get_str(str1, str2);
     if (str1[strlen(str1) - 1] != '\n')
         return -1;
@@ -114,7 +115,10 @@ int main()
     int n1 = split_str(str1, *str1_words);
     if (n1 == 0)
         return -4;
-    int n2 = split_str(str2, *str2_words);
+    if (strlen(str2) > 1)
+        n2 = split_str(str2, *str2_words);
+    else
+        n2 = 0;
     remove_duplicates(str1_words, &n1);
     check(str1_words, str2_words, n1, n2);
     return 0;
