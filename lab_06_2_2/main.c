37a746ebe1fdba1839950b7a2cf48087999be5b3
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-06-06 16:39:59 +0300

    Memory fix #2 in second program

--- lab_06_2_2/main.c
+++ lab_06_2_2/main.c
@@ -3,12 +3,11 @@
 #define N 258
 #define K 17
 
-void get_str(char *str1, char *str2)
+void get_str(char *str1)
 {
     FILE *f;
     f = stdin;
     fgets(str1, N, f);
-    fgets(str2, N, f);
     return;
 }
 
@@ -108,30 +107,30 @@ void check(char str1_words[N][K], char str2_words[N][K], int n1, int n2)
 int main()
 {
     char str1[N];
-    char str2[N];
-    int n2;
-    get_str(str1, str2);
+    get_str(str1);
     if (str1[strlen(str1) - 1] != '\n')
         return -1;
-    if (str2[strlen(str2) - 1] != '\n')
-        return -2;
     if (strlen(str1) == 1)
-        return -3;
+        return -2;
     char str1_words[N][K];
     int n1 = split_str(str1, *str1_words);
     if (n1 == 0)
-    {
-        printf("-4");
-        return -4;
-    }
+        return -3;
     remove_duplicates(str1_words, &n1);
-    if (strlen(str2) <= 1)
+    char str2[N];
+    get_str(str2);
+    if (str2[strlen(str2) - 1] != '\n')
+        return -4;
+    if (strlen(str2) == 1)
         print_without_check(str1_words, n1);
     else
     {
         char str2_words[N][K];
-        n2 = split_str(str2, *str2_words);
-        check(str1_words, str2_words, n1, n2);
+        int n2 = split_str(str2, *str2_words);
+        if (n2 == 0)
+            print_without_check(str1_words, n1);
+        else
+            check(str1_words, str2_words, n1, n2);
     }
     return 0;
 }
