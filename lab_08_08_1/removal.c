8681f44bb428c98ba69e9c0793c28706bae4c740
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-09-20 22:51:11 +0300

    removal.c became old.

--- lab_08_08_1/removal.c
+++ lab_08_08_1/removal.c
@@ -3,27 +3,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-/*int comp(double u1, double *p1, double *p2)
-{
-    if (fabs(u1 - fabs(*p1)) > fabs(u1 - fabs(*p2)))
-        return 1;
-    return 0;
-}*/
-
-void slip(int *n, int j, double *p)
-{
-    for (int i = j; i < *n - 1; i++)
-    {
-        *p = *(p + 1);
-        p++;
-    }
-    return;
-}
-
 void del(double *a, int *n, double u1)
 {
     double *k1, *k2, *p = a;
-    //if (comp(u1, p, p + 1) == 1)
+    int j;
     if (fabs(u1 - fabs(*p)) > fabs(u1 - fabs(*(p + 1))))
     {
         k1 = p;
@@ -36,10 +19,8 @@ void del(double *a, int *n, double u1)
     }
     for (int i = 2; i < *n; i++)
     {
-        //if (comp(u1, p + i, k2) == 1)
         if (fabs(u1 - fabs(*(p + i))) > fabs(u1 - fabs(*k2)))
         {
-            //if (comp(u1, p + i, k1) == 1)
             if (fabs(u1 - fabs(*p + i)) > fabs(u1 - fabs(*k1)))
                 k1 = p + i;
             else
@@ -47,13 +28,17 @@ void del(double *a, int *n, double u1)
         }
     }
     p = a;
-    int j = 0;
+    j = 0;
     while (p < k1)
     {
         p++;
         j++;
     }
-    slip(n, j, p);
+    for (int i = j; i < *n - 1; i++)
+    {
+        *p = *(p + 1);
+        p++;
+    }
     *n = *n - 1;
     if (k2 > k1)
         k2--;
@@ -66,7 +51,11 @@ void del(double *a, int *n, double u1)
         p++;
         j++;
     }
-    slip(n, j, p);
+    for (int i = j; i < *n - 1; i++)
+    {
+        *p = *(p + 1);
+        p++;
+    }
     *n = *n - 1;
     return;
 }
