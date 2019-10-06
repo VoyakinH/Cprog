0173c114f4571188aa5e06307221c4c18e6f2c8b
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-10-06 23:08:49 +0300

    first func test added

--- lab_09_06_01/unit_tests.c
+++ lab_09_06_01/unit_tests.c
@@ -9,19 +9,25 @@ int read_two_int_must_return_cor_res()
     int expected[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     int count = 0;
     int x, y;
-    for (int i = 0; i < 10; i += 2)
+    FILE *buff = fopen("buff.txt", "a+");
+    for (int i = 0; i < 9; i += 2)
     {
-        fprintf(stdin, "%d %d", a[i], a[i + 1]);
-        read_two_int(stdin, &x, &y);
+        fprintf(buff, "%d %d ", a[i], a[i + 1]);
+    }
+    fseek(buff, 0, SEEK_SET);
+    for (int i = 0; i < 9; i += 2)
+    {
+        read_two_int(buff, &x, &y);
         if (x == expected[i] && y == expected[i + 1])
             count++;
     }
-    if (count == 10)
-        printf("\read_two_int: OK\n");
+    fclose(buff);
+    if (count == 5)
+        printf("read_two_int: OK\n");
     else
         printf("read_two_int: Failed\n");
     printf("Tests passed %d of 10\n\n", count);
-    if (count == 10)
+    if (count == 5)
         return 1;
     return 0;
 }
