9ea1092fabc9303b94b9769af4aa65a166996bd7
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-10-06 23:17:48 +0300

    Style fixed

--- lab_09_06_01/unit_tests.c
+++ lab_09_06_01/unit_tests.c
@@ -3,13 +3,13 @@
 #include "func.h"
 #include "defines.h"
 
-int read_two_int_must_return_cor_res()
+int read_two_int_work_cor()
 {
-    int a[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    int expected[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int a[10] = { -1, 0, 300, 401, 505, 6, 7, 8, 9, 10 };
+    int expected[10] = { -1, 0, 300, 401, 505, 6, 7, 8, 9, 10 };
     int count = 0;
     int x, y;
-    FILE *buff = fopen("buff.txt", "a+");
+    FILE *buff = fopen("buff.txt", "w+");
     for (int i = 0; i < 9; i += 2)
     {
         fprintf(buff, "%d %d ", a[i], a[i + 1]);
@@ -26,7 +26,36 @@ int read_two_int_must_return_cor_res()
         printf("read_two_int: OK\n");
     else
         printf("read_two_int: Failed\n");
-    printf("Tests passed %d of 10\n\n", count);
+    printf("Tests passed %d of 5\n\n", count);
+    if (count == 5)
+        return 1;
+    return 0;
+}
+
+int read_two_power_work_cor()
+{
+    int a[10] = { -1, 0, 300, 401, 505, 6, 7, 8, 9, 10 };
+    int expected[10] = { -1, 0, 300, 401, 505, 6, 7, 8, 9, 10 };
+    int count = 0;
+    int x, y;
+    FILE *buff = fopen("buff.txt", "w+");
+    for (int i = 0; i < 9; i += 2)
+    {
+        fprintf(buff, "%d %d ", a[i], a[i + 1]);
+    }
+    fseek(buff, 0, SEEK_SET);
+    for (int i = 0; i < 9; i += 2)
+    {
+        read_two_power(buff, &x, &y);
+        if (x == expected[i] && y == expected[i + 1])
+            count++;
+    }
+    fclose(buff);
+    if (count == 5)
+        printf("read_two_int: OK\n");
+    else
+        printf("read_two_int: Failed\n");
+    printf("Tests passed %d of 5\n\n", count);
     if (count == 5)
         return 1;
     return 0;
@@ -35,7 +64,8 @@ int read_two_int_must_return_cor_res()
 int main()
 {
     int count = 0;
-    count += read_two_int_must_return_cor_res();
-    printf("Test functions passed %d of 1\n", count);
+    count += read_two_int_work_cor();
+    count += read_two_power_work_cor();
+    printf("Test functions passed %d of 2\n", count);
     return 0;
 }
