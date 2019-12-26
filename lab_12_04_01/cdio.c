a54bf1ca79dbdd31e07cd08450b2f32543fcb7b6
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-12-26 18:18:19 +0300

    Checks for input added

--- lab_12_04_01/cdio.c
+++ lab_12_04_01/cdio.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "defines.h"
 
 int read_mode(FILE *f, char mode[])
 {
-    if (fgets(mode, 4, f) == NULL)
+    if (fgets(mode, 5, f) == NULL)
         return READ_MODE_ERR;
+    if (mode[3] != '\n')
+        return READ_MODE_ERR;
+    mode[3] = '\0';
     return OK;
 }
 
 int read_int(FILE *f, int *val)
 {
     if (fscanf(f, "%d", val) != 1 || *val < 1)
+        return READ_INT_ERR;
+    char buff = fgetc(f);
+    if (buff != '\n' && buff != ' ')
         return READ_INT_ERR;
     return OK;
 }
--- lab_12_04_01/main.c
+++ lab_12_04_01/main.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    char mode[4];
+    char mode[5];
     if (read_mode(FILE_NAME, mode) != OK)
         return READ_MODE_ERR;
     return check_mode(mode);
